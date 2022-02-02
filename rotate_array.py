bcc47d47f19984b24aa267804f5b673945868ef5
akashshuklacs <akashshuklacse@gmail.com>
2022-02-02 20:26:19 +0530

    added qn 189

---
+++ rotate_array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k%len(nums)
+        l, r =0, len(nums)-1
+        while l<r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l+=1
+            r-=1
+        
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
